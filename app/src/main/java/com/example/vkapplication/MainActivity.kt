4370f210978f311544bfd0386b9a6ef40b9af837
Timasoska <Tim005rud@yandex.ru>
2024-03-12 12:30:17 +0300

    plus

--- app/src/main/java/com/example/vkapplication/MainActivity.kt
+++ app/src/main/java/com/example/vkapplication/MainActivity.kt
@@ -5,51 +5,125 @@ import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.graphics.translationMatrix
 import com.example.vkapplication.ui.theme.RowModel
 import com.example.vkapplication.ui.theme.Sites
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                Column(Modifier.fillMaxSize()){
-                    Row(modifier = Modifier
-                        .background(Color.LightGray)
-                        .fillMaxWidth()
-                    ) {
-                        Text(
-                            modifier = Modifier.padding(10.dp),
-                            text = "Passwords",
-                            style = TextStyle(
-                                fontWeight = FontWeight.Bold,
-                                fontStyle = FontStyle.Italic,
-                                fontSize = 18.sp
-                            )
-                        )
-                    }
-                    RowModel(item = Sites(R.drawable.telegram,"telegram","dfdf","kfgmk"))
+            Row(
+                modifier = Modifier
+                    .background(Color.LightGray)
+                    .fillMaxWidth()
+            ) {
+                Text(
+                    modifier = Modifier.padding(10.dp),
+                    text = "Passwords",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontStyle = FontStyle.Italic,
+                        fontSize = 18.sp
+                    )
+                )
+            }
+            LazyColumn(Modifier.fillMaxSize()) {
+                itemsIndexed(
+                    liftOf(
+
+                    )
+                ){_, item ->
+                    RowModel(item = item)
                 }
+
+                //RowModel(item = Sites(R.drawable.telegram, "telegram", "dfdf", "kfgmk"))
+            }
+
+            Box(modifier = Modifier
+                .fillMaxHeight()
+                .padding(10.dp),
+                contentAlignment = Alignment.TopStart)
+            {
+                fun onClick() {
+
+                }
+                FloatingActionButton(
+                    //Засунуть всё в скалфорд
+                    onClick = { onClick() },
+                    shape = CircleShape,
+                    containerColor = Color.Blue,
+                    contentColor = Color.White,
+                    elevation = FloatingActionButtonDefaults.elevation(48.dp),
+
+                    ) {
+                    Icon(Icons.Filled.Add, "button")
+                }
+            }
+
+        /*    fun onClick() {
+
+            }
+            FloatingActionButton(
+                //Засунуть всё в скалфорд
+                onClick = { onClick() },
+                shape = CircleShape,
+                containerColor = Color.Blue,
+                contentColor = Color.White,
+                elevation = FloatingActionButtonDefaults.elevation(48.dp),
+
+                ) {
+                Icon(Icons.Filled.Add, "button")
+            }*/
         }
-    }
+
+    } // floating Action button сразу уедет куда надо
 }
 
--- app/src/main/java/com/example/vkapplication/ui/theme/RowModel.kt
+++ app/src/main/java/com/example/vkapplication/ui/theme/RowModel.kt
@@ -1,6 +1,7 @@
 package com.example.vkapplication.ui.theme
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -11,13 +12,22 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.time.format.TextStyle
 
 @Composable
 fun RowModel(item: Sites){
-    Row(modifier = Modifier.fillMaxWidth()) {
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .clickable (onClick = {})
+        .shadow(1.dp)
+    ){
         Image(
             painter = painterResource(id = item.icon),
             contentDescription = null,
@@ -30,15 +40,20 @@ fun RowModel(item: Sites){
         Column {
             Text(
                 text = item.title,
+                fontSize = 20.sp,
+                fontWeight = FontWeight.Bold,
                 modifier = Modifier
                     .padding(5.dp)
             )
             Text( //Сделать серый цвет
                 text = item.login,
+                color = Color.DarkGray,
+                fontSize = 15.sp,
                 modifier = Modifier
-                    .padding(5.dp)
+                    .padding(5.dp, 3.dp)
                 )
         }
     }
-
 }
+
+//fun onclickItem()
