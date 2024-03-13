9cab1dc356cc7a27bd0d41fe358ea2e976bd6597
Timofey Rudakov <Tim005rud@gmail.com>
2024-03-13 16:34:52 +0300

    2Й ЭКРАН

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-
+    implementation ("androidx.compose.material:material:1.6.3")
     implementation ("androidx.navigation:navigation-compose:2.7.7")
     implementation ("io.coil-kt:coil-compose:1.4.0")
     implementation("androidx.core:core-ktx:1.9.0")
--- app/src/main/java/com/example/vkapplication/ui/theme/Screens.kt
+++ app/src/main/java/com/example/vkapplication/ui/theme/Screens.kt
@@ -1,6 +1,8 @@
 package com.example.vkapplication.ui.theme
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -12,37 +14,122 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.vkapplication.SampleData
+import kotlinx.coroutines.launch
 
+
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Screen2(navController: NavHostController) {
+
     Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp)
     ) {
-        Text(text = "SCREEN2")
+        var login by remember { mutableStateOf("")}
+        var password by rememberSaveable { mutableStateOf("")}
+        var url by remember { mutableStateOf("")}
+        val coroutineScope = rememberCoroutineScope()
+        val scaffoldState = rememberScaffoldState()
+
+        OutlinedTextField(
+            value = login,
+            onValueChange = {login = it},
+            label = {Text("Логин")},
+            modifier = Modifier.padding(20.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Email,
+                    contentDescription = null
+                )
+            }
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = {password = it},
+            label = {Text("Пароль")},
+            modifier = Modifier.padding(20.dp),
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Lock,
+                    contentDescription = null
+                )
+            }
+        )
+        OutlinedTextField(
+            value = url,
+            onValueChange = {url = it},
+            label = {Text("Веб-сайт")},
+            modifier = Modifier.padding(20.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Uri),
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Filled.Search,
+                    contentDescription = null
+                )
+            }
+        )
+
+        Button(onClick = {
+            if (login.isEmpty() && password.isNotEmpty()) {
+                coroutineScope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar(
+                        message = "Логин пустой",
+                        duration = SnackbarDuration.Long
+                    )
+                }
+            }
+        }) {
+            Text("Нажми")
+        }
+
     }
 }
+
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -93,3 +180,4 @@ fun MainScreen(navController: NavHostController){
         }
     )
 }
+
